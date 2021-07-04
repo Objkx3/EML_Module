53bf247f48c355ed40aab6a96c6ffa80504c7d7b
Miano Mashiro <84467961+Objkx3@users.noreply.github.com>
2021-07-04 13:53:03 +0200

    Create .lua

---
+++ .lua
@@ -0,0 +1,80 @@
+DarkRP.createEntity("Gas", {
+    ent = "eml_gas",
+    model = "models/props_c17/canister01a.mdl",
+    price = 800,
+    max = 4,
+    cmd = "Gas"
+    category = "EML"
+})
+
+DarkRP.createEntity("Gazinière", {
+    ent = "eml_stove",
+    model = "models/props_c17/furnitureStove001a.mdl",
+    price = 3000,
+    max = 2,
+    cmd = "Gazinière"
+    category = "EML"
+})
+
+DarkRP.createEntity("Spécial Pot", {
+    ent = "eml_spot",
+    model = "models/props_c17/metalPot001a.mdl",
+    price = 1000,
+    max = 2,
+    cmd = "Bpot",
+    category = "EML"
+})
+
+DarkRP.createEntity("Pot", {
+    ent = "eml_pot",
+    model = "models/props_c17/metalPot001a.mdl",
+    price = 1000,
+    max = 2,
+    cmd = "Pot",
+    category = "EML"
+})
+
+DarkRP.createEntity("Jar (EML)", {
+    ent = "eml_jar",
+    model = "models/props_lab/jar01a.mdl",
+    price = 500,
+    max = 2,
+    cmd = "JarEML",
+    category = "EML"
+})
+
+DarkRP.createEntity("Muriatic Acid", {
+    ent = "eml_macid",
+    model = "models/props_junk/garbage_plasticbottle001a.mdl",
+    price = 80,
+    max = 6,
+    cmd = "Muriaticacid",
+    category = "EML"
+})
+
+DarkRP.createEntity("Liquide Lodine", {
+    ent = "eml_iodine",
+    model = "models/props_lab/jar01b.mdl",
+    price = 85,
+    max = 6,
+    cmd = "Iodine",
+    category = "EML"
+})
+
+DarkRP.createEntity("Water", {
+    ent = "eml_water",
+    model = "models/props_junk/garbage_plasticbottle003a.mdl",
+    price = 100,
+    max = 6,
+    cmd = "Water",
+    category = "EML"
+})
+
+DarkRP.createEntity("Liquide Sulfur", {
+    ent = "eml_sulfur",
+    model = "models/props_lab/jar01b.mdl",
+    price = 110,
+    max = 6,
+    cmd = "Sulfur",
+    category = "EML"
+})
